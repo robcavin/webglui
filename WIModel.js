3c071ee0a5631a30120e1c2558e8737b1a4826c5
Rob Cavin <rob@lightt.com>
2013-10-11 00:00:30 -0700

    Initial commit

--- WIModel.js
+++ WIModel.js
@@ -3,6 +3,7 @@
  */
 function WIModel(webGLUI) {
 
+    this.webGLUI = webGLUI;
     this.gl = webGLUI.gl;
     this.vertexPositionBuffer = null;
     this.shaderProgram = null;
@@ -11,31 +12,32 @@ function WIModel(webGLUI) {
     return this;
 }
 
-WIModel.prototype.configure = function (vertices, itemSize, numItems) {
-    var gl = this.gl;
-    var vertexPositionBuffer = this.vertexPositionBuffer;
-    
-    vertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    vertexPositionBuffer.itemSize = itemSize;
-    vertexPositionBuffer.numItems = numItems;
-}
+WIModel.prototype = {
+    init: function (vertices, itemSize, numItems, drawMode) {
+        var gl = this.gl;
 
-WIModel.prototype.draw = function () {
-    var gl = this.gl;
-    var vertexPositionBuffer = this.vertexPositionBuffer;
-    var shaderProgram = this.shaderProgram;
-    var drawMode = this.drawMode;
+        var vertexPositionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        vertexPositionBuffer.itemSize = itemSize;
+        vertexPositionBuffer.numItems = numItems;
 
-    if (!drawMode || !shaderProgram) {
-        alert('The shader program and draw mode for the model must be set before calling draw.')
-        return;
+        this.vertexPositionBuffer = vertexPositionBuffer;
+        this.drawMode = drawMode;
+        return this;
+    },
+
+    draw: function (pMatrix, mvMatrix) {
+
+        var gl = this.gl;
+        var vertexPositionBuffer = this.vertexPositionBuffer;
+        var shaderProgram = this.shaderProgram || this.webGLUI.defaultShaderProgram;
+        var drawMode = this.drawMode || gl.TRIANGLES;
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+        gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+        gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+        gl.drawArrays(drawMode, 0, vertexPositionBuffer.numItems);
     }
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-    gl.drawArrays(this.drawMode, 0, vertexPositionBuffer.numItems);
 }
--- WIScene.js
+++ WIScene.js
@@ -3,4 +3,41 @@
  */
 function WIScene(webGLUI) {
     this.gl = webGLUI.gl;
+    this.models = [];
+    this.mvMatrix = mat4.create();
+    this.pMatrix = mat4.create();
+
+    return this;
+}
+
+WIScene.prototype = {
+
+    addModel: function (model) {
+        this.models.push(model)
+    },
+
+    update: function () {
+        var gl = this.gl;
+
+        var pMatrix = this.pMatrix;
+        mat4.perspective(pMatrix, 45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
+
+        var mvMatrix = this.mvMatrix;
+        mat4.identity(mvMatrix);
+        mat4.translate(mvMatrix, mvMatrix, [-1.5, 0.0, -7.0]);
+
+    },
+
+    draw: function () {
+        var pMatrix = this.pMatrix;
+        var mvMatrix = this.mvMatrix;
+        var gl = this.gl;
+
+        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        this.models.forEach(function (model) {
+            model.draw(pMatrix, mvMatrix)
+        })
+    }
 }
